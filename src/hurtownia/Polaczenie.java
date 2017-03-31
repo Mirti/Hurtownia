d5a1c33b9bde4ecfc5f8a5107e8b9142ad28a376
Marcin <avalith@o2.pl>
2017-03-31 17:36:55 +0200

    Dodałem moduł logowania

--- src/hurtownia/Polaczenie.java
+++ src/hurtownia/Polaczenie.java
@@ -7,44 +7,32 @@ package hurtownia;
 
 import java.sql.*;
 
-
 public class Polaczenie {
-    
+
     private Connection con;
     private Statement st;
     private ResultSet rs;
-    
-    public Polaczenie (){
-        try{
+
+    public Polaczenie() {
+        try {
+
             Class.forName("com.mysql.jdbc.Driver");
-            con = DriverManager.getConnection("jdbc:mysql://localhost:3306/hurtownia","root","");
-            st  = con.createStatement();
-            
-            
-            
-        }catch(Exception ex){
-            System.out.println("Błąd"+ ex);
+        } catch (ClassNotFoundException a) {
+            System.out.print("zjebane");
+        }
+        try {
+            con = DriverManager.getConnection("jdbc:mysql://localhost:3306/hurtowniaspozywcza", "root", "");
+            st = con.createStatement();
+            System.out.print("ok");
+
+        } catch (Exception ex) {
+            System.out.println("Błąd" + ex);
         }
     }
-    
-    /*
-        public void getData(){
-          try{
-              String query = "select * from produkt";
-              rs = st.executeQuery(query);
-              System.out.println("rekordy z bazy danych");
-              while(rs.next()){
-                  String nazwa = rs.getString("nazwa");
-                  System.out.println("nazwa:"+nazwa);
-                  
-              }
-              
-          }catch(Exception ex){
-              System.out.println(ex);
-          } 
-        }
-     */   
+
+    public ResultSet getData(String query) throws SQLException {
+        rs = st.executeQuery(query);
+        return rs;
+
     }
-    
-
-
+}

062b20a18808ebfd31abe0464dde5ed362a89ce5
cmptr <cmptr@192.168.0.16>
2017-05-07 17:10:26 +0200

    modul  przyjecia towaru

--- src/hurtownia/InDAO.java
+++ src/hurtownia/InDAO.java
@@ -20,7 +20,7 @@ public class InDAO {
     //*******************************
     public static ObservableList<In> searchIn () throws SQLException, ClassNotFoundException {
         //Declare a SELECT statement
-        String selectStmt = "select p.produkt_temp_id,p.nazwa,p.cena_jednostkowa,x.data_przyjecia,p.ilosc,p.polozenie,p.dostawca,p.kraj_pochodzenia from produkt_temp p,przyjecie x where p.przyjecie_id=x.przyjecie_id";
+        String selectStmt = "SELECT nazwa, cena_jednostkowa, data_waznosci, ilosc, polozenie, dostawca, kraj_pochodzenia FROM `produkt_temp`";
  
         //Execute SELECT statement
         try {
@@ -46,10 +46,9 @@ public class InDAO {
  
         while (rs.next()) {
             In in = new In();
-            in.setInId(rs.getInt("produkt_temp_id"));
             in.setInName(rs.getString("nazwa"));
             in.setInPrice(rs.getString("cena_jednostkowa"));
-            in.setInDate(rs.getDate("data_przyjecia"));
+            in.setInDate(rs.getDate("data_waznosci"));
             in.setInQuantity(rs.getInt("ilosc"));
             in.setInPosition(rs.getString("polozenie"));
             in.setInProvider(rs.getString("dostawca"));

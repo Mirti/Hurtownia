af5705d494101d32db1a5bb0887a6327c158a386
Artur <kijoszeq12@gmail.com>
2017-05-25 17:29:13 +0200

    dodaje rezerwacje produktow w magazynie na zamowienie

--- src/hurtownia/Polaczenie.java
+++ src/hurtownia/Polaczenie.java
@@ -1,4 +1,4 @@
-/*
+﻿/*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
@@ -34,7 +34,7 @@ public class Polaczenie {
         } catch (ClassNotFoundException a) {
         }
         try {
-            con = DriverManager.getConnection("jdbc:mysql://localhost:3307/hurtowniaspozywcza", "root", "");
+            con = DriverManager.getConnection("jdbc:mysql://localhost:3306/hurtowniaspozywcza", "root", "");
             st = con.createStatement();
             System.out.print("ok");
 

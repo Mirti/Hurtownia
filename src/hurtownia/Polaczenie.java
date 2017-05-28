ef0f8eff312d3d55e269af2d7c6acdd128552d26
Artur <kijoszeq12@gmail.com>
2017-05-28 15:58:44 +0200

    zmieniam porty

--- src/hurtownia/Polaczenie.java
+++ src/hurtownia/Polaczenie.java
@@ -1,4 +1,4 @@
-/*
+﻿/*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
@@ -39,7 +39,7 @@ public class Polaczenie {
         } catch (ClassNotFoundException a) {
         }
         try {
-            con = DriverManager.getConnection("jdbc:mysql://localhost:3307/hurtowniaspozywcza", "root", "");
+            con = DriverManager.getConnection("jdbc:mysql://localhost:3306/hurtowniaspozywcza", "root", "");
             st = con.createStatement();
             System.out.print("ok");
 
@@ -73,7 +73,7 @@ public class Polaczenie {
     }
 
     public static void importDB() throws IOException, FileNotFoundException, SQLException {
-        Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3307/?user=root&password=");
+        Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/?user=root&password=");
         Statement s = con.createStatement();
         s.executeUpdate("CREATE DATABASE hurtowniaspozywcza");
         s.close();
@@ -83,7 +83,7 @@ public class Polaczenie {
         fileChooser.setTitle("Open Resource File");
         String path = fileChooser.showOpenDialog(stage).getAbsolutePath();
         System.out.print(path);
-        con = DriverManager.getConnection("jdbc:mysql://localhost:3307/hurtowniaspozywcza", "root", "");
+        con = DriverManager.getConnection("jdbc:mysql://localhost:3306/hurtowniaspozywcza", "root", "");
         Polaczenie.runScript(path, con);
         Alert alert = new Alert(Alert.AlertType.INFORMATION);
         alert.setTitle("Potwierdzenie utworzenia bazy");

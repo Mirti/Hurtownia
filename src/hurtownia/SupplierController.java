a58e707241f3ba63dd80a558fb321167cb3c239c
Artur <kijoszeq12@gmail.com>
2017-06-04 16:26:53 +0200

    Dodawanie Klienta

--- src/hurtownia/SupplierController.java
+++ src/hurtownia/SupplierController.java
@@ -13,6 +13,9 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.text.Text;
 
@@ -45,6 +48,14 @@ public class SupplierController implements Initializable {
     @FXML
     private Text txt;
     @FXML
+    private Label CPromptCargo;
+    @FXML
+    private Label CPromptDisc;
+    @FXML
+    private CheckBox CClient;
+    @FXML
+    private TextField CDisc;
+    @FXML
     private Button Ok;
     
     /**
@@ -61,8 +72,20 @@ public class SupplierController implements Initializable {
         String tel = CTel.getText();
         String eMail = CEmail.getText();
         String towar = CCargo.getText();
+        String znizka = CDisc.getText();
         Connect con = new Connect();
-                
+        
+        if(CClient.isSelected()){
+            if(nazwa.equals("")||(adres.equals(""))||(wlasciciel.equals(""))||(tel.equals(""))||(eMail.equals(""))||(znizka.equals("")))
+        {txt.setText("Wypełnij wymagane pola!");}
+        else
+        {
+            String query = "insert into klient(nazwa,wlasciciel,adres,telefon,email,rabat) "
+                    + "VALUES(\"" + nazwa + "\",\"" + wlasciciel + "\",\"" + adres + "\",\"" + tel + "\",\"" + eMail + "\", \"" + znizka + "\")";
+            con.update(query);
+            
+        }
+        }   else{
         if(nazwa.equals("")||(adres.equals(""))||(wlasciciel.equals(""))||(tel.equals(""))||(eMail.equals(""))||(towar.equals("")))
         {txt.setText("Wypełnij wymagane pola!");}
         else
@@ -74,6 +97,15 @@ public class SupplierController implements Initializable {
         }
  
 }
- 
+    }
+ public void block(ActionEvent e) {
+        if (e.getSource() == CClient) {
+            CDisc.setVisible(CClient.isSelected());
+            CPromptDisc.setVisible(CClient.isSelected());
+            CCargo.setVisible(!CClient.isSelected());
+            CPromptCargo.setVisible(!CClient.isSelected());
+            
+        }
+    }
     
 }
